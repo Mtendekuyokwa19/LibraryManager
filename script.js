e1e5af754891d6da8ed2b1241ea85471539a5f73
Mtende <mtendekuyokwa19@gmail.com>
2023-11-07 16:20:16 +0200

    creating the UI

---
+++ script.js
@@ -0,0 +1,26 @@
+// const myLibrary = [];
+
+// function Book() {
+//   // the constructor...
+// }
+
+// //addBook will run code that add book and will run the contructorafter taking in entries from users
+
+// //the submit button return everything in form of a form so make add books be able to receives the result in the form
+
+// function addBookToLibrary() {
+//   // do stuff here
+// }
+
+
+// //a button for creating a book it will create a form
+
+let newEntry=document.getElementById("compose");
+
+let dialog=document.getElementById("entryBox");
+
+newEntry.addEventListener("click", () => {
+    dialog.showModal();
+
+  })
+
